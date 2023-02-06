fce022b12e85bb877d308c6c201f3c92542b1adc
keremakaynak <kerem.kaynak@code.berlin>
2023-02-06 23:23:38 +0100

    fix: logger

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // CommonJs
-const fastify = require('fastify')()
+const fastify = require('fastify')({
+  logger: true
+})
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'kero2' }
@@ -13,6 +15,7 @@ const PORT = parseInt(parseInt(process.env.PORT)) || 8080
 const HOST = parseInt(parseInt(process.env.HOST)) || '0.0.0.0'
 
 const start = async () => {
-    await fastify.listen({ port: PORT, host: HOST })  
+    await fastify.listen({ port: PORT, host: HOST })
+    logger.info(`Server is now listening on port: ${PORT}`)
 }
 start()
