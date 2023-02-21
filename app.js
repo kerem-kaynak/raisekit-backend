111060fe7b249d194fd8a11e02668301048390d6
keremakaynak <kerem.kaynak@code.berlin>
2023-02-21 12:55:29 +0100

    feat: mrr breakdown endpoints

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const fastify = require('fastify')({
 	prettyPrint: true,
 	disableRequestLogging: false
 })
-const { calculateMRR, calculateARR } = require('./helpers/metrics/metrics')
+const { 
+	calculateMRR, 
+	calculateARR,
+	calculateNewMRR,
+	calculateChurnedMRR,
+	calculateContractionMRR,
+	calculateExpansionMRR
+} = require('./helpers/metrics/metrics')
 
 fastify.get('/', async (req, res) => {
 
@@ -38,6 +45,63 @@ fastify.route({
 		}
 	}
 })
+
+fastify.route({
+	method: 'POST',
+	url: '/api/v0/metrics/new_mrr',
+	handler: async function (req, res) {
+		try {
+			const result = await calculateNewMRR(req.body)
+			res.status(200).send(result)
+		} catch (err) {
+			fastify.log.error(err)
+			res.send(500)
+		}
+	}
+})
+
+fastify.route({
+	method: 'POST',
+	url: '/api/v0/metrics/churned_mrr',
+	handler: async function (req, res) {
+		try {
+			const result = await calculateChurnedMRR(req.body)
+			res.status(200).send(result)
+		} catch (err) {
+			fastify.log.error(err)
+			res.send(500)
+		}
+	}
+})
+
+fastify.route({
+	method: 'POST',
+	url: '/api/v0/metrics/contraction_mrr',
+	handler: async function (req, res) {
+		try {
+			const result = await calculateContractionMRR(req.body)
+			res.status(200).send(result)
+		} catch (err) {
+			fastify.log.error(err)
+			res.send(500)
+		}
+	}
+})
+
+fastify.route({
+	method: 'POST',
+	url: '/api/v0/metrics/expansion_mrr',
+	handler: async function (req, res) {
+		try {
+			const result = await calculateExpansionMRR(req.body)
+			res.status(200).send(result)
+		} catch (err) {
+			fastify.log.error(err)
+			res.send(500)
+		}
+	}
+})
+
 /**
  * Run the server!
  */
--- helpers/metrics/metrics.js
+++ helpers/metrics/metrics.js
@@ -1,6 +1,11 @@
-const calculateMRR = async (df) => {
+const generateTimeArray = (df) => {
 	var timeSeries = Object.keys(df[0])
 	timeSeries.shift()
+	return timeSeries
+}
+
+const calculateMRR = async (df) => {
+	const timeSeries = generateTimeArray(df)
 	var mrrSeries = []
 	timeSeries.forEach((element)=>{
 		var sum = 0
@@ -23,4 +28,77 @@ const calculateARR = async (df) => {
 	return arrSeries
 }
 
-module.exports = { calculateMRR, calculateARR }
+const calculateNewMRR = async (df) => {
+	const timeSeries = generateTimeArray(df)
+	var newMrrSeries = []
+	for (let i = 1; i < timeSeries.length; i++) {
+		var sum = 0
+		let currentTimeframe
+		for (let j = 0; j < df.length; j++) {
+			currentTimeframe = timeSeries[i]
+			const currentMrr = parseFloat(df[j][timeSeries[i]])
+			const previousMrr = parseFloat(df[j][timeSeries[i-1]])
+			sum += ((previousMrr === 0) && (currentMrr !== 0)) ? (currentMrr - previousMrr) : 0
+		}
+		const newMrrDatapoint = {[currentTimeframe]: sum}
+		newMrrSeries.push(newMrrDatapoint)
+	}
+	return newMrrSeries
+}
+
+const calculateChurnedMRR = async (df) => {
+	const timeSeries = generateTimeArray(df)
+	var churnedMrrSeries = []
+	for (let i = 1; i < timeSeries.length; i++) {
+		var sum = 0
+		let currentTimeframe
+		for (let j = 0; j < df.length; j++) {
+			currentTimeframe = timeSeries[i]
+			const currentMrr = parseFloat(df[j][timeSeries[i]])
+			const previousMrr = parseFloat(df[j][timeSeries[i-1]])
+			sum += ((previousMrr !== 0) && (currentMrr === 0)) ? (currentMrr - previousMrr) : 0
+		}
+		const churnedMrrDatapoint = {[currentTimeframe]: sum}
+		churnedMrrSeries.push(churnedMrrDatapoint)
+	}
+	return churnedMrrSeries
+}
+
+const calculateContractionMRR = async (df) => {
+	const timeSeries = generateTimeArray(df)
+	var contractionMrrSeries = []
+	for (let i = 1; i < timeSeries.length; i++) {
+		var sum = 0
+		let currentTimeframe
+		for (let j = 0; j < df.length; j++) {
+			currentTimeframe = timeSeries[i]
+			const currentMrr = parseFloat(df[j][timeSeries[i]])
+			const previousMrr = parseFloat(df[j][timeSeries[i-1]])
+			sum += ((previousMrr > currentMrr) && (currentMrr !== 0)) ? (currentMrr - previousMrr) : 0
+		}
+		const contractionMrrDatapoint = {[currentTimeframe]: sum}
+		contractionMrrSeries.push(contractionMrrDatapoint)
+	}
+	return contractionMrrSeries
+}
+
+const calculateExpansionMRR = async (df) => {
+	const timeSeries = generateTimeArray(df)
+	var expansionMrrSeries = []
+	for (let i = 1; i < timeSeries.length; i++) {
+		var sum = 0
+		let currentTimeframe
+		for (let j = 0; j < df.length; j++) {
+			currentTimeframe = timeSeries[i]
+			const currentMrr = parseFloat(df[j][timeSeries[i]])
+			const previousMrr = parseFloat(df[j][timeSeries[i-1]])
+			sum += ((previousMrr < currentMrr) && (previousMrr !== 0)) ? (currentMrr - previousMrr) : 0
+		}
+		const expansionMrrDatapoint = {[currentTimeframe]: sum}
+		expansionMrrSeries.push(expansionMrrDatapoint)
+	}
+	return expansionMrrSeries
+}
+
+
+module.exports = { calculateMRR, calculateARR, calculateNewMRR, calculateChurnedMRR, calculateContractionMRR, calculateExpansionMRR }
