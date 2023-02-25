d12416b11145e20628dffba66af4543ef8115625
keremakaynak <kerem.kaynak@code.berlin>
2023-02-25 21:02:45 +0100

    feat: net dollar retention endpoint

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const {
 	calculateNetMrrChurnRate,
 	calculateGrossMrrChurnRate,
 	calculateLogoRetentionRate,
-	calculateLogoChurnRate
+	calculateLogoChurnRate,
+	calculateNetDollarRetention
 } = require('./helpers/metrics/metrics')
 
 fastify.get('/', async (req, res) => {
@@ -242,6 +243,20 @@ fastify.route({
 	handler: async function (req, res) {
 		try {
 			const result = await calculateGrossMrrChurnRate(req.body)
+			res.status(200).send(result)
+		} catch (err) {
+			fastify.log.error(err)
+			res.send(500)
+		}
+	}
+})
+
+fastify.route({
+	method: 'POST',
+	url: '/api/v0/metrics/net_dollar_retention',
+	handler: async function (req, res) {
+		try {
+			const result = await calculateNetDollarRetention(req.body)
 			res.status(200).send(result)
 		} catch (err) {
 			fastify.log.error(err)
